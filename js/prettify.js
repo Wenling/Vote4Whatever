7ec77ef51f42e6fd7c3c40be2222fd1ec6345c35
Wenling Shi <swling29@gmail.com>
2012-12-11 23:32:22 -0500

    Project framework finished
    Used bootstrap for UI, has add Category and list Category right now

---
+++ js/prettify.js
@@ -0,0 +1,40 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1138\cocoasubrtf510
+{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red188\green0\blue0;\red7\green127\blue137;\red34\green0\blue255;
+\red5\green101\blue0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl260
+
+\f0\fs22 \cf0 .com \{ \cf2 color\cf0 : \cf3 #93a1a1\cf0 ; \}\
+.lit \{ \cf2 color\cf0 : \cf3 #195f91\cf0 ; \}\
+.pun, .opn, .clo \{ \cf2 color\cf0 : \cf3 #93a1a1\cf0 ; \}\
+.fun \{ \cf2 color\cf0 : \cf3 #dc322f\cf0 ; \}\
+.str, .atv \{ \cf2 color\cf0 : \cf3 #D14\cf0 ; \}\
+.kwd, .prettyprint .tag \{ \cf2 color\cf0 : \cf3 #1e347b\cf0 ; \}\
+.typ, .atn, .dec, .var \{ \cf2 color\cf0 : \cf3 teal\cf0 ; \}\
+.pln \{ \cf2 color\cf0 : \cf3 #48484c\cf0 ; \}\
+\
+.prettyprint \{\
+  \cf2 padding\cf0 : \cf4 8px\cf0 ;\
+  \cf2 background-color\cf0 : \cf3 #f7f7f9\cf0 ;\
+  \cf2 border\cf0 : \cf4 1px\cf0  \cf3 solid\cf0  \cf3 #e1e1e8\cf0 ;\
+\}\
+.prettyprint.linenums \{\
+  \cf2 -webkit-box-shadow\cf0 : \cf3 inset\cf0  \cf4 40px\cf0  \cf4 0\cf0  \cf4 0\cf0  \cf3 #fbfbfc\cf0 , \cf3 inset\cf0  \cf4 41px\cf0  \cf4 0\cf0  \cf4 0\cf0  \cf3 #ececf0\cf0 ;\
+     -moz-box-shadow: \cf3 inset\cf0  \cf4 40px\cf0  \cf4 0\cf0  \cf4 0\cf0  \cf3 #fbfbfc\cf0 , \cf3 inset\cf0  \cf4 41px\cf0  \cf4 0\cf0  \cf4 0\cf0  \cf3 #ececf0\cf0 ;\
+          \cf2 box-shadow\cf0 : \cf3 inset\cf0  \cf4 40px\cf0  \cf4 0\cf0  \cf4 0\cf0  \cf3 #fbfbfc\cf0 , \cf3 inset\cf0  \cf4 41px\cf0  \cf4 0\cf0  \cf4 0\cf0  \cf3 #ececf0\cf0 ;\
+\}\
+\
+\pard\pardeftab720\sl260
+\cf5 /* Specify class=linenums on a pre to get line numbering */\cf0 \
+ol.linenums \{\
+  \cf2 margin\cf0 : \cf4 0\cf0  \cf4 0\cf0  \cf4 0\cf0  \cf4 33px\cf0 ; \cf5 /* IE indents via margin-left */\cf0 \
+\}\
+ol.linenums li \{\
+  \cf2 padding-left\cf0 : \cf4 12px\cf0 ;\
+  \cf2 color\cf0 : \cf3 #bebec5\cf0 ;\
+  \cf2 line-height\cf0 : \cf4 20px\cf0 ;\
+  \cf2 text-shadow\cf0 : \cf4 0\cf0  \cf4 1px\cf0  \cf4 0\cf0  \cf3 #fff\cf0 ;\
+\}\
+}
--- voting.py
+++ voting.py
@@ -12,58 +12,80 @@ import os
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 
-class Greeting(db.Model):
-    """Models an individual Guestbook entry with an author, content, and date."""
-    author = db.StringProperty()
-    content = db.StringProperty(multiline=True)
-    date = db.DateTimeProperty(auto_now_add=True)
+#category has ancestor user
+class Category(db.Model):
+    name = db.CategoryProperty()
+    create_time = db.DateTimeProperty(auto_now_add=True)
+    expiration_time = db.DateTimeProperty()
 
+#item has ancestor category
+class Item(db.Model):
+    name = db.StringProperty()
+    create_time = db.DateTimeProperty(auto_now_add=True)
+    picture = db.BlobProperty(default='img/ny.jpg')
 
-def guestbook_key(guestbook_name=None):
+#vote has ancestor category
+class Vote(db.Model):
+    voter = db.Key()
+    voted_item = db.StringProperty()
+    unvoted_item = db.StringProperty()
+    vote_time = db.DateTimeProperty(auto_now_add=True)
+
+class Comment(db.Model):
+    commenter = db.Key()
+    item = db.Key()
+    create_time = db.DateTimeProperty()
+    content = db.StringProperty()
+
+class AddCat(webapp2.RequestHandler):
+    def post(self):
+        cat_name = self.request.get('cat_name')
+        cat_expt = datetime.datetime(2013, 11, 1, 1)
+        
+        user_id = user_key(users.get_current_user().user_id())
+        if users.get_current_user():
+            cat = Category(parent=user_id, name=cat_name, expiration_time=cat_expt)
+            cat.put()
+#self.response.out.write(user_id)
+            self.redirect('/cat_name?' + urllib.quote(cat_name))
+
+class AddItem(webapp2.RequestHandler):
+    def get(self):
+        user_id = user_key(users.get_current_user().user_id())
+        self.response.out.write(user_id)
+#self.redirect('/?' + urllib.urlencode({'cat_name': cat_name}))
+
+def user_key(user_id=None):
     """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
-    return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
-
+    return db.Key.from_path('UserId', user_id or 'default_user')
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        guestbook_name=self.request.get('guestbook_name')
-        greetings_query = Greeting.all().ancestor(guestbook_key(guestbook_name)).order('-date')
-        greetings = greetings_query.fetch(10)
         
-        if users.get_current_user():
+        template_values = {}
+        user = users.get_current_user()
+        if user:
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
+            username = user.nickname()
+            user_id = user.user_id()
+
+            category_query = Category.all()
+            category_query.ancestor(user_key(user_id))
+            cats = category_query.run()
+            template_values['categories'] = cats
+            
         else:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
+            username = ''
         
-        template_values = {
-            'greetings': greetings,
-            'url': url,
-            'url_linktext': url_linktext,
-        }
-        
+        template_values['url'] = url
+        template_values['url_linktext'] = url_linktext
+        template_values['username'] = username
+                
         template = jinja_environment.get_template('index.html')
         self.response.out.write(template.render(template_values))
 
 
-class Guestbook(webapp2.RequestHandler):
-    def post(self):
-        # We set the same parent key on the 'Greeting' to ensure each greeting is in
-        # the same entity group. Queries across the single entity group will be
-        # consistent. However, the write rate to a single entity group should
-        # be limited to ~1/second.
-        guestbook_name = self.request.get('guestbook_name')
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
-        
-        if users.get_current_user():
-            greeting.author = users.get_current_user().nickname()
-        
-        greeting.content = self.request.get('content')
-        greeting.put()
-        self.redirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
-
-
-app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/sign', Guestbook)],
-                              debug=True)
+app = webapp2.WSGIApplication([('/', MainPage), ('/addCat', AddCat), ('/cat_name', AddItem)], debug=True)
