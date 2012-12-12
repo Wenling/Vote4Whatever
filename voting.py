f359e45da2e367f535b77e1f70c48ed763078d9a
Wenling Shi <swling29@gmail.com>
2012-12-12 16:22:15 -0500

    finally add item and list item works

--- voting.py
+++ voting.py
@@ -5,6 +5,7 @@ import webapp2
 
 from google.appengine.ext import db
 from google.appengine.api import users
+from google.appengine.api import images
 
 import jinja2
 import os
@@ -42,11 +43,8 @@ class Comment(db.Model):
 def user_key(user_id=None):
     return db.Key.from_path('UserId', user_id or 'default_user')
 
-def user_key(cat_id=None):
-    return db.Key.from_path('CatId', cat_id)
-
-def user_key(item_id=None):
-    return db.Key.from_path('ItemId', item_id)
+def cat_key(user_id=None, cat_name=None):
+    return db.Key.from_path('UserId', user_id, 'CatId', cat_name)
 
 """These functions are the models' manipulations"""
 #insert a new category
@@ -54,7 +52,7 @@ def insertCat(user_id, cat_name):
     cat_expt = datetime.datetime(2013, 11, 1, 1)
     cat = Category(parent=user_id, key_name=cat_name, name=cat_name, expiration_time=cat_expt)
     cat.put()
-    return cat.key()
+    return cat
 
 #list all categories under the user
 def listCat(query):
@@ -75,7 +73,7 @@ def insertItem(user_id, cat_id, item_name, pic_dir):
         if pic_dir:
             item.picture = db.Blob(open(pic_dir, "rb").read())
         item.put()
-        return item.key()
+        return item
 
 #list all items under the category
 def listItem(query):
@@ -94,19 +92,23 @@ class AddCat(webapp2.RequestHandler):
         
         if users.get_current_user():
             user_id = user_key(users.get_current_user().user_id())
-            cat_id = insertCat(user_id, cat_name)
+            cat = insertCat(user_id, cat_name)
             #self.response.out.write(cat_id)
-            self.redirect('/?' + urllib.urlencode({'cat_id': cat_id}))
+            self.redirect('/?' + urllib.urlencode({'cat_name': cat.name}))
 
 class AddItem(webapp2.RequestHandler):
     def post(self):
-        user_id = user_key(users.get_current_user().user_id())
+        user_id = users.get_current_user().user_id()
         #self.response.out.write(user_id)
-        cat_id = self.request.get('cat_id')
+        cat_name = self.request.get('cat_name')
+#self.response.out.write(cat_name)
+        cat_id = cat_key(user_id, cat_name)
         item_name = self.request.get('item_name')
         pic_dir = self.request.get('picture')
-        item_id = insertItem(user_id, cat_id, item_name, pic_dir)
-        self.redirect('/?' + urllib.urlencode({'cat_id': cat_id}))
+        item = insertItem(user_key(user_id), cat_id, item_name, pic_dir)
+#self.response.out.write(item.name)
+        
+        self.redirect('/?' + urllib.urlencode({'parent': cat_name}) + '&'+ urllib.urlencode({'item_name': item_name}))
 
 
 class Dispatcher(webapp2.RequestHandler):
@@ -126,10 +128,16 @@ class Dispatcher(webapp2.RequestHandler):
                     list = listCat(query)
                     template_values['categories'] = list
             
-                elif self.request.get('cat_id'):
-                    query = {'ancestor' : user_key(self.request.get('cat_id'))}
+                elif self.request.get('cat_name'):
+                    cat_name = self.request.get('cat_name')
+                    cat_id = cat_key(user_id, cat_name)
+                    query = {'ancestor' : cat_id}
                     list = listItem(query)
                     template_values['items'] = list
+                    template_values['cat_name'] = cat_name
+            
+                elif self.request.get('item_name'):
+                    cat_name = self.request.get('parent')
         
             else:
                 query = { 'ancestor' :user_key(user_id) }
